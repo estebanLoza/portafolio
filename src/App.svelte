e40391e30589a459582662ab475cd422f7834f2e
estebanLoza <estebanm.hloza@gmail.com>
2024-07-04 12:28:07 -0600

    Este es un nuevo proyecto con Svelte

---
+++ src/App.svelte
@@ -0,0 +1,41 @@
+<script>
+	export let name;
+	let otroNombre = "esteban";
+	
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<h2>HELLO {otroNombre}</h2>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	 <p>ESTO ESTA ESCRITO DESDE TERMUX </p>
+	  <ul>
+	    <li>Esto es una lista de prueba</li>
+	    <li>Esto es una lista de prueba</li>
+	    <li>Esto es una lista de prueba</li>
+	    <li>Esto es una lista de prueba</li>
+	  </ul>
+  
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
