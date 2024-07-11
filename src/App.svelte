5a9286f8d46f9ea6054321ac00fc43714d8fa0a2
estebanLoza <estebanm.hloza@gmail.com>
2024-07-11 13:01:48 -0600

    jasd

--- src/App.svelte
+++ src/App.svelte
@@ -7,8 +7,8 @@
 	<h1>Esteba Hernandez</h1>
 	<p>Soy estudiante de Ingeniería en Mecatrónica con una gran pasión por la electrónica y la robótica. Además, me interesa profundamente la programación, abarcando tanto el desarrollo web y backend como la creación de proyectos de software. He participado en hackathones, lo que me ha permitido identificar áreas de mejora en mis estudios y reconocer mis avances. El primero de ellos ha sido salir de mi zona de miedo y confort. He creado proyectos personales de manera autodidacta, simples pero efectivos, y siempre con el objetivo de aprender </p>
 	
-	
-	
+	  <h2>asdfsdf</h2>
+	 <p>lksjfsñldf</p>
 	
 	
 </main>
